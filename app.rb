bf1b47a958a9a32acedf77ae4a6f76e45b9ac0f5
Muhammadreza Haghiri <haghiri75@gmail.com>
2016-08-01 10:53:30 +0430

    API

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+
+#############################################
+#   IAUCTB Scientific Association of        #
+#        Computer Engineering               #
+#             Backend                       #
+#############################################
+
+get '/' do
+
+end
+
+get '/introduction' do
+
+end
+
+get '/boardofdirectors' do
+
+end
+
+get '/conffeed' do
+
+end
